60fae4c99ff4cf20a601ab5c50597fe6c4f9dd70
Hiswe <hiswehalya@gmail.com>
2018-09-29 12:41:24 +0700

    GAME – begin to make a web prototype

---
+++ src/main.js
@@ -0,0 +1,53 @@
+import Phaser from 'phaser'
+
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  parent: `game-container`,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 },
+    },
+  },
+  scene: {
+    preload,
+    create,
+  },
+}
+
+var game = new Phaser.Game(config)
+
+function preload() {
+  this.load.setBaseURL('http://labs.phaser.io')
+
+  this.load.image('sky', 'assets/skies/space3.png')
+  this.load.image('logo', 'assets/sprites/phaser3-logo.png')
+  this.load.image('red', 'assets/particles/red.png')
+}
+
+function create() {
+  // - images are centered based positioned
+  // - you can change origin by calling
+  //   this.add.image(0, 0, 'sky').setOrigin(0, 0)
+  //   https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.Image.html#setOrigin__anchor
+  // - they stack in order of appearance
+  this.add.image(400, 300, 'sky')
+
+  var particles = this.add.particles('red')
+
+  var emitter = particles.createEmitter({
+    speed: 100,
+    scale: { start: 1, end: 0 },
+    blendMode: 'ADD',
+  })
+
+  var logo = this.physics.add.image(400, 100, 'logo')
+
+  logo.setVelocity(100, 200)
+  logo.setBounce(1, 1)
+  logo.setCollideWorldBounds(true)
+
+  emitter.startFollow(logo)
+}
